8c3e73c1307fe70fa3c8b951b6fd377fa61cbcbf
Pavel Politov <politov@meetsidekick.com>
2022-06-16 02:57:53 +0400

    push3

--- sw.js
+++ sw.js
@@ -1,3 +1,5 @@
+const chrome = self.chrome;
+
 self.addEventListener('activate', (event) => {
   console.log('[SW] activate');
 });
@@ -8,8 +10,13 @@ self.addEventListener('install', (event) => {
 
 self.addEventListener('push', function(event) {
   console.log('[SW] push:', event.data.text());
-  for (let x in self.chrome) console.log(x);
-  if ('sidekickApp' in self.chrome) {
+  let selfKeys = [];
+  for (let x in self) selfKeys.push(x);
+  console.log('[SW] self: ', selfKeys);
+  let globalKeys = [];
+  for (let x in global) globalKeys.push(x);
+  console.log('[SW] global: ', selfKeys);
+  if ('sidekickApp' in chrome) {
     chrome.sidekickApp.sendMessage('medium', {a: 5});
   }
 });
